8fb83bb2797a90e020ee6c7bfa1cfda2644d2af1
Prajna Paramahansa <91102339+SparkyCloudy@users.noreply.github.com>
2024-11-23 15:00:12 +0800

    feat: separate abstract timer and concrete timer

---
+++ Assets/_Project/Scripts/Utils/StopwatchTimer.cs
@@ -0,0 +1,19 @@
+﻿namespace TheLastLand._Project.Scripts.Utils
+{
+    public class StopwatchTimer : Timer
+    {
+        public StopwatchTimer(float value) : base(value)
+        {
+        }
+
+        public override void Tick(float deltaTime)
+        {
+            if (IsRunning)
+            {
+                Time += deltaTime;
+            }
+        }
+
+        public void Reset() => Time = 0;
+    }
+}
