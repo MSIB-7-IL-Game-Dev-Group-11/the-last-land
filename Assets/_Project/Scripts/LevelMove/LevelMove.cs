b1b54d9dc070cd1c2d19b646aed0c494d0261625
Prajna Paramahansa <91102339+SparkyCloudy@users.noreply.github.com>
2024-11-26 13:15:29 +0800

    chore: fix indentation

--- Assets/_Project/Scripts/LevelMove/LevelMove.cs
+++ Assets/_Project/Scripts/LevelMove/LevelMove.cs
@@ -6,36 +6,36 @@ using UnityEngine.SceneManagement;
 namespace TheLastLand
 {
     public class LevelMove : MonoBehaviour
-{
-    public string targetScene = "Desa_B"; // Nama scene tujuan
-    private bool isPlayerInRange = false; // Cek apakah player dalam jangkauan
-
-    void Update()
     {
-        // Jika player berada dalam jangkauan dan menekan tombol 'M'
-        if (isPlayerInRange && Input.GetKeyDown(KeyCode.M))
+        public string targetScene = "Desa_B"; // Nama scene tujuan
+        private bool isPlayerInRange = false; // Cek apakah player dalam jangkauan
+
+        void Update()
         {
-            // Pindah ke scene tujuan
-            SceneManager.LoadScene(targetScene);
+            // Jika player berada dalam jangkauan dan menekan tombol 'M'
+            if (isPlayerInRange && Input.GetKeyDown(KeyCode.M))
+            {
+                // Pindah ke scene tujuan
+                SceneManager.LoadScene(targetScene);
+            }
+        }
+
+        // Ketika pemain masuk dalam collider gapura
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                isPlayerInRange = true;
+            }
+        }
+
+        // Ketika pemain keluar dari collider gapura
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                isPlayerInRange = false;
+            }
         }
     }
-
-    // Ketika pemain masuk dalam collider gapura
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            isPlayerInRange = true;
-        }
-    }
-
-    // Ketika pemain keluar dari collider gapura
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            isPlayerInRange = false;
-        }
-    }
-}
 }
