eb929f8113ac0eab8e355209beb2d0581757ad0b
Prajna Paramahansa <91102339+SparkyCloudy@users.noreply.github.com>
2024-11-30 14:58:16 +0800

    feat: add item script

---
+++ Assets/_Project/Scripts/Item.cs
@@ -0,0 +1,19 @@
+﻿using TheLastLand._Project.Scripts.GameSystems.Item;
+using TheLastLand._Project.Scripts.GameSystems.Item.Common;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace TheLastLand._Project.Scripts
+{
+    public class Item : MonoBehaviour, ICollectible
+    {
+        [SerializeField] private ItemData itemData;
+        public event UnityAction<ItemData> OnItemCollected = delegate { };
+        
+        public void Collect()
+        {
+            Destroy(gameObject);
+            OnItemCollected?.Invoke(itemData);
+        }
+    }
+}
