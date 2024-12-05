dab5da354218e01bea1c53c122e584dd82900c4b
Prajna Paramahansa <91102339+SparkyCloudy@users.noreply.github.com>
2024-12-06 01:24:43 +0800

    feat: add LoadAssetIfNull extension method to GameObjectExtensions class

--- Assets/_Project/Scripts/Extensions/GameObjectExtensions.cs
+++ Assets/_Project/Scripts/Extensions/GameObjectExtensions.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 namespace TheLastLand._Project.Scripts.Extensions
 {
@@ -17,5 +18,32 @@ namespace TheLastLand._Project.Scripts.Extensions
         /// <param name="obj">The object being checked.</param>
         /// <returns>The object itself if it exists and not destroyed, null otherwise.</returns>
         public static T OrNull<T>(this T obj) where T : Object => obj ? obj : null;
+
+        /// <summary>
+        /// Loads an asset from the specified path if the provided asset is null.
+        /// </summary>
+        /// <typeparam name="T">The type of the asset to load, which must be a ScriptableObject.</typeparam>
+        /// <param name="obj">The object calling this extension method.</param>
+        /// <param name="asset">The asset to check and potentially load.</param>
+        /// <param name="path">The path to load the asset from if it is null.</param>
+        /// <returns>The loaded asset if it was null, otherwise the original asset.</returns>
+        /// <remarks>
+        /// This method helps ensure that a ScriptableObject asset is loaded from a specified path if it is not already assigned.
+        /// If the asset is null, it attempts to load it from the given path using the AssetDatabase.
+        /// If the asset cannot be loaded, an error message is logged.
+        /// </remarks>
+        public static T LoadAssetIfNull<T>(this Object obj, T asset, string path)
+            where T : ScriptableObject
+        {
+            if (asset != null) return asset;
+
+            asset = AssetDatabase.LoadAssetAtPath<T>(path);
+            if (asset == null)
+            {
+                Debug.LogError($"Failed to load {typeof(T).Name} from {path}!");
+            }
+
+            return asset;
+        }
     }
 }
