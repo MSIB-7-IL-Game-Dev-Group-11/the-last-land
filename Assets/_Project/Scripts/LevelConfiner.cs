892c52279a7c4ed23940beb60156734819e6fb21
Prajna Paramahansa <91102339+SparkyCloudy@users.noreply.github.com>
2024-12-09 12:33:40 +0800

    feat: add MainSystem Manager

---
+++ Assets/_Project/Scripts/LevelConfiner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace TheLastLand._Project.Scripts
+{
+    public class LevelConfiner : MonoBehaviour
+    {
+        public static event Action<Collider2D> OnConfinerEnter = delegate { };
+
+        private void Start()
+        {
+            OnConfinerEnter?.Invoke(GetComponent<PolygonCollider2D>());
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Managers/MainSystemManager.cs
@@ -0,0 +1,54 @@
+﻿using Cinemachine;
+using TheLastLand._Project.Scripts.Extensions;
+using TheLastLand._Project.Scripts.SeviceLocator;
+using UnityEngine;
+
+namespace TheLastLand._Project.Scripts.Managers
+{
+    public class MainSystemManager : MonoBehaviour
+    {
+        private static MainSystemManager _instance;
+
+        [SerializeField] private CinemachineVirtualCamera virtualCamera;
+        private CinemachineConfiner2D _confiner;
+
+        private void Awake()
+        {
+            if (_instance == null)
+            {
+                Initialize();
+            }
+            else if (_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void OnEnable()
+        {
+            LevelConfiner.OnConfinerEnter += SetConfiner;
+        }
+
+        private void OnDisable()
+        {
+            LevelConfiner.OnConfinerEnter -= SetConfiner;
+        }
+
+        private void Initialize()
+        {
+            _instance = this;
+            ServiceLocator.Global.RegisterServiceIfNotExists(_instance);
+            DontDestroyOnLoad(gameObject);
+
+            _confiner = virtualCamera.GetComponent<CinemachineConfiner2D>();
+        }
+
+        private void SetConfiner(Collider2D newConfiner)
+        {
+            if (_confiner != null)
+            {
+                _confiner.m_BoundingShape2D = newConfiner;
+            }
+        }
+    }
+}
