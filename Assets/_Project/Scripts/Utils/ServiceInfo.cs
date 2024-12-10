4fb9c2eb4e4d82d2d7a5335e29f5a2f01bbdd1eb
Prajna Paramahansa <91102339+SparkyCloudy@users.noreply.github.com>
2024-12-11 03:04:36 +0800

    feat: add logger for service locator

---
+++ Assets/_Project/Scripts/Utils/ServiceInfo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace TheLastLand._Project.Scripts.Utils
+{
+    public static class ServiceInfo
+    {
+        public static void LogServiceRetrieval(string className, string serviceName, bool success)
+        {
+            Debug.Log(
+                success
+                    ? $"{className} successfully retrieved {serviceName} service."
+                    : $"{className} failed to retrieve {serviceName} service."
+            );
+        }
+    }
+}
