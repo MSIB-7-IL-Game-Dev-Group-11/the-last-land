8a7bcacb43c435d538737b118863cf39a87ac96e
Prajna Paramahansa <91102339+SparkyCloudy@users.noreply.github.com>
2024-12-11 03:08:08 +0800

    feat: add item manager

---
+++ Assets/_Project/Scripts/Managers/ItemManager.cs
@@ -0,0 +1,30 @@
+﻿using TheLastLand._Project.Scripts.Extensions;
+using TheLastLand._Project.Scripts.SeviceLocator;
+using UnityEngine;
+
+namespace TheLastLand._Project.Scripts.Managers
+{
+    public class ItemManager : MonoBehaviour
+    {
+        private static ItemManager _instance;
+
+        private void Awake()
+        {
+            if (_instance == null)
+            {
+                Initialize();
+            }
+            else if (_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void Initialize()
+        {
+            _instance = this;
+            ServiceLocator.Global.RegisterServiceIfNotExists(_instance);
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
