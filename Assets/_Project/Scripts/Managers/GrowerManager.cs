986b8b99ffe2a7bbfef4f72d92823c9d25011603
Prajna Paramahansa <91102339+SparkyCloudy@users.noreply.github.com>
2024-12-11 05:18:12 +0800

    update assets with unfinished manager

---
+++ Assets/_Project/Scripts/Managers/GrowerManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace TheLastLand._Project.Scripts.Managers
+{
+    public class GrowerManager : MonoBehaviour
+    {
+        // unfinished script
+        
+        private static GrowerManager _instance;
+
+        private void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
